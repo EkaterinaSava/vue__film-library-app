f437f2b198a94a1b1b8ecba9b20162bb61f42f9d
EkaterinaSava <ek@weare.lt>
2018-11-22 15:00:09 +0100

    Lession #1: Init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  #app
+    h1 Hello
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+  #app
+    padding 30px
+    background-color #333
+    color #fff
+</style>

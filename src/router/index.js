ec1d3dc3f0de20e7964a64187aba2ba8a88c9c54
EkaterinaSava <ek@weare.lt>
2018-11-22 15:14:04 +0100

    ADD "UImini" library for styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Uimini from 'uimini/dist/css/uimini.css'
 
 Vue.use(Router)
+Vue.use(Uimini)
 
 export default new Router({
   routes: []

89c1221f192d3d82a0e542044e8dd5fd5fcbc165
EkaterinaSava <ek@weare.lt>
2018-11-28 11:48:09 +0100

    ADD store/task & ADD Task.vue computed:tasks

---
+++ src/store/task.js
@@ -0,0 +1,29 @@
+export default {
+  state: {
+    tasks: [
+      {
+        'id': 1,
+        'title': 'GrowthBusters: Hooked on Growth',
+        'description': 'I directed this documentary challenging the myths linking growth with prosperity and fulfillment. It explores how our beliefs about economic and consumption',
+        'whatWatch': 'Film',
+        'completed': false,
+        'editing': false
+      },
+      {
+        'id': 2,
+        'title': 'Game of thrones',
+        'description': 'Best serials',
+        'whatWatch': 'Serial',
+        'completed': false,
+        'editing': false
+      }
+    ]
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    tasks (state) {
+      return state.tasks
+    }
+  }
+}

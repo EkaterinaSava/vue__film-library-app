8965320bed051729219ec5e9f1bc086688dbf8d7
EkaterinaSava <ek@weare.lt>
2018-11-30 14:30:56 +0100

    ADD firebase in project

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+
 import App from './App'
 import router from './router'
 import store from './store'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/messaging'
+import 'firebase/storage'
 
 Vue.config.productionTip = false
 
@@ -16,5 +23,18 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+
+  // Initialize Firebase
+  created () {
+    var config = {
+      apiKey: 'AIzaSyAi0oV06-njKCAFrB_t_Q1b06qzFmH2XGI',
+      authDomain: 'film-library-by-sava.firebaseapp.com',
+      databaseURL: 'https://film-library-by-sava.firebaseio.com',
+      projectId: 'film-library-by-sava',
+      storageBucket: 'film-library-by-sava.appspot.com',
+      messagingSenderId: '283848193269'
+    }
+    firebase.initializeApp(config)
+  }
 })

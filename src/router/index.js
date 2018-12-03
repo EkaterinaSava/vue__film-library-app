171ca2fb9b67d659c5ded3743424cb62d1feedce
EkaterinaSava <ek@weare.lt>
2018-12-03 12:10:37 +0100

    ADD logic for not show Home & Tasks pages if user not logined

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Uimini from 'uimini/dist/css/uimini.css'
+import store from '../store'
 
 import Home from '@/components/Home'
 import Task from '@/components/Task'
@@ -13,9 +14,36 @@ Vue.use(Uimini)
 export default new Router({
   mode: 'history',
   routes: [
-    {path: '/', name: 'home', component: Home},
-    {path: '/task', name: 'task', component: Task},
-    {path: '/login', name: 'login', component: Login},
-    {path: '/registration', name: 'registration', component: Registration}
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      beforeEnter (to, from, next) {
+        store.getters.checkUser ? next() : next('/login')
+        // if (store.getters.checkUser) {
+        //   next()
+        // } else {
+        //   next('/login')
+        // }
+      }
+    },
+    {
+      path: '/task',
+      name: 'task',
+      component: Task,
+      beforeEnter (to, from, next) {
+        store.getters.checkUser ? next() : next('/login')
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: Registration
+    }
   ]
 })
